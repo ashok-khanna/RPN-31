29fb0f53ec7aea5e551f935244f68be6eddc5257
premier-education <ashok.khanna@hotmail.com>
2020-05-16 23:05:38 +0400

    Initial Commit

---
+++ RPN-31/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RPN-31
+//
+//  Created by Ashok Khanna on 16/5/20.
+//  Copyright © 2020 Ashok Khanna. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
